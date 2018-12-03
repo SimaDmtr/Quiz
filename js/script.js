595f58076b0e9c8c4ca04dadce42972d8a63ace0
Maxim Dmitruk <39312751+SimaDmtr@users.noreply.github.com>
2018-12-03 17:32:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(function() {
-    var blockTop = $('.other_countries').offset().top;
+$(function () {
+    var blockTop = $('.other_countries .want_iqra_button').offset().top;
     var CountUpFlag = 0;
     var $window = $(window);
-    $window.on('load scroll', function() {
+    $window.on('load scroll', function () {
         var top = $window.scrollTop();
         var height = $window.height();
         if (top + height >= blockTop && CountUpFlag == 0) {
@@ -10,23 +10,36 @@ $(function() {
             CountUpFlag = 1;
         }
     });
-    function CountUp() {
-        $('.other_countries').addClass('back_effect');
-    }
-});
-$(function() {
-    var blockTop = $('.want_iqra_button').offset().top;
-    var CountUpFlag = 0;
-    var $window = $(window);
-    $window.on('load scroll', function() {
-        var top = $window.scrollTop();
-        var height = $window.height();
-        if (top + height >= blockTop && CountUpFlag == 0) {
-            CountUp();
-            CountUpFlag = 1;
-        }
-    });
+
     function CountUp() {
         $('.other_countries').addClass('logo_effect');
     }
 });
+$('.change_region_trigger').click(function () {
+    $('.change_region_modal').addClass('active')
+})
+$(".location").hover(function () {
+    if ($('.country_guess').hasClass("active")) {
+        $('.country_guess').fadeOut().removeClass("active");
+        $('.location .icon-arrow-down').removeClass("active");
+    } else {
+        $('.country_guess').fadeIn().addClass("active");
+        $('.location .icon-arrow-down').addClass("active");
+    }
+})
+$(document).mouseup(function (e) {
+    var container = $(".change_region_modal");
+    if (container.has(e.target).length === 0) {
+        container.removeClass('active');
+    }
+});
+
+var k = 0;
+var slow_rate = 14;
+
+function back() {
+    k--;
+    $('.background').css('backgroundPosition-y', k / slow_rate);
+}
+
+setInterval(back, 10)
