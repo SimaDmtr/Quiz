526bc0142f20497bd902da8258992f2213308e23
Maxim Dmitruk <39312751+SimaDmtr@users.noreply.github.com>
2018-12-06 14:37:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
-$(function () {
-    var blockTop = $('.other_countries .want_iqra_button').offset().top;
-    var CountUpFlag = 0;
-    var $window = $(window);
-    $window.on('load scroll', function () {
-        var top = $window.scrollTop();
-        var height = $window.height();
-        if (top + height >= blockTop && CountUpFlag == 0) {
-            CountUp();
-            CountUpFlag = 1;
-        }
+new WOW().init();
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: new google.maps.LatLng(55.1896363, 30.2748985),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+
     });
 
-    function CountUp() {
-        $('.other_countries').addClass('logo_effect');
+    var brest = {lat: 52.0873776, lng: 23.6377467};
+    var bar = {lat: 52.9159423, lng: 25.2228568};
+    var moskow = {lat: 55.902707, lng: 37.614874};
+    var tver = {lat: 56.914908, lng: 35.913904};
+    var positions = [brest, bar, moskow, tver]
+    for (var i = 0; i < positions.length; i++) {
+        var marker = new google.maps.Marker({
+            position: positions[i],
+            map: map,
+            icon: {
+                url: "images/logo.png",
+                scaledSize: new google.maps.Size(48, 48)
+            }
+        });
     }
-});
+    var i;
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+
+    }
+}
 $('.change_region_trigger').click(function () {
     $('.change_region_modal').addClass('active')
 })
@@ -22,7 +45,7 @@ $(".location").hover(function () {
     if ($('.country_guess').hasClass("active")) {
         $('.country_guess').fadeOut().removeClass("active");
         $('.location .icon-arrow-down').removeClass("active");
-    } else {
+    } else  {
         $('.country_guess').fadeIn().addClass("active");
         $('.location .icon-arrow-down').addClass("active");
     }
@@ -33,13 +56,21 @@ $(document).mouseup(function (e) {
         container.removeClass('active');
     }
 });
+            //табы франшизы
+$(".franch_tabs_content").not(":first").hide(300)
+$(".franch_tabs_content:first").addClass("active");
+$(".franch_tabs_nav_item").click(function () {
+    $(".franch_tabs_nav_item").eq($(this).index()).addClass("active");
+    $(".franch_tabs_content").hide().eq($(this).index()).fadeIn().addClass('active').siblings().removeClass('active');
+    $(this).siblings().removeClass('active  ')}).eq(0).addClass("active");
 
+
+                //движение фона
 var k = 0;
 var slow_rate = 14;
-
 function back() {
     k--;
     $('.background').css('backgroundPosition-y', k / slow_rate);
 }
-
 setInterval(back, 10)
+
