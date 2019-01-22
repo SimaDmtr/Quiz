6d074f70388da55d0a70a3318880ecc6f53ed4b4
Maxim Dmitruk <39312751+SimaDmtr@users.noreply.github.com>
2019-01-22 14:01:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: new google.maps.LatLng(55.1896363, 30.2748985),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+    });
+
+    var brest = {lat: 52.0873776, lng: 23.6377467};
+    var bar = {lat: 52.9159423, lng: 25.2228568};
+    var moskow = {lat: 55.902707, lng: 37.614874};
+    var tver = {lat: 56.914908, lng: 35.913904};
+    var positions = [brest, bar, moskow, tver]
+    for (var i = 0; i < positions.length; i++) {
+        var marker = new google.maps.Marker({
+            position: positions[i],
+            map: map,
+            icon: {
+                url: "images/logo.png",
+                scaledSize: new google.maps.Size(48, 48)
+            }
+        });
+    }
+
+}
+
+
+
 (function ($) {
     wow = new WOW(
         {
@@ -6,45 +34,7 @@
     )
     wow.init();
 
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 6,
-            center: new google.maps.LatLng(55.1896363, 30.2748985),
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
 
-        });
-
-        var brest = {lat: 52.0873776, lng: 23.6377467};
-        var bar = {lat: 52.9159423, lng: 25.2228568};
-        var moskow = {lat: 55.902707, lng: 37.614874};
-        var tver = {lat: 56.914908, lng: 35.913904};
-        var positions = [brest, bar, moskow, tver]
-        for (var i = 0; i < positions.length; i++) {
-            var marker = new google.maps.Marker({
-                position: positions[i],
-                map: map,
-                icon: {
-                    url: "images/logo.png",
-                    scaledSize: new google.maps.Size(48, 48)
-                }
-            });
-        }
-        var i;
-        for (i = 0; i < locations.length; i++) {
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                map: map
-            });
-
-            google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
-                    infowindow.setContent(locations[i][0]);
-                    infowindow.open(map, marker);
-                }
-            })(marker, i));
-
-        }
-    }
 
     $('.fotorama__img').click()
     $('.change_region_trigger').click(function () {
